72925b32a336c46485863f188c4611476963d2c0
HaShiRaMaReiZo <zwemannhtet65@gmail.com>
2025-12-24 17:15:37 +0630

    Auth with clerk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,34 @@
 import "../../global.css"
-import { Slot, Stack, Tabs } from "expo-router";
+import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { useAuth } from "@clerk/clerk-expo";
+import { ActivityIndicator } from "react-native";
 
-export default function RootLayout(){
-    const isAuthenticated = true; // Replace with your authentication logic
-    return <Stack screenOptions={{headerShown: false}}>
-        <Stack.Protected guard={!isAuthenticated}>
+
+function RootStack(){
+    const { isSignedIn , isLoaded} = useAuth();
+
+    if(!isLoaded){
+        return <ActivityIndicator />;
+    }
+    return(
+        <Stack screenOptions={{headerShown: false}}>
+        <Stack.Protected guard={!isSignedIn}>
             <Stack.Screen name="(auth)" />
         </Stack.Protected>
-        <Stack.Protected guard={isAuthenticated}>
+        <Stack.Protected guard={!!isSignedIn}>
         <Stack.Screen name="(drawer)"  />
         </Stack.Protected>
-    </Stack>;
+    </Stack>
+    );
+}
+export default function RootLayout(){
+    return (
+        <ClerkProvider 
+        publishableKey={"pk_test_ZmxleGlibGUtbGFkeWJ1Zy0yOS5jbGVyay5hY2NvdW50cy5kZXYk"}
+        tokenCache={tokenCache} >
+            <RootStack />
+    </ClerkProvider>
+    );
 };
